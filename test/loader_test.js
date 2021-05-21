646ddec9ad4d7b3591929ab253ab1296ed23a753
Felipe Silva <felipe.silva@ultreia.com.br>
2021-05-21 09:49:34 -0300

    Add css to  component

--- test/loader_test.js
+++ test/loader_test.js
@@ -9,80 +9,48 @@ export default class loaderTest {
     const body = document.querySelector("body");
 
     if (body.style.overflow === "hidden") {
-      console.log(
-        "%c✓ body is locked",
-        "padding: 0 15px;background: #000; color: #bada55"
-      );
-    } else {
-      console.log(
-        "%c✗ body is not locked",
-        "padding: 0 15px;background: #000; color: red"
-      );
+      console.log("%c✓ body is locked","padding: 0 15px;background: #000; color: #bada55");
+    } 
+    else {
+      console.log("%c✗ body is not locked","padding: 0 15px;background: #000; color: red");
     }
 
     if (loader.style.right === "0px") {
-      console.log(
-        "%c✓ loader in position",
-        "padding: 0 15px;background: #000; color: #bada55"
-      );
-    } else {
-      console.log(
-        "%c✗ loader is not in position",
-        "padding: 0 15px;background: #000; color: red"
-      );
+      console.log("%c✓ loader in position","padding: 0 15px;background: #000; color: #bada55");
+    }
+     else {
+      console.log("%c✗ loader is not in position","padding: 0 15px;background: #000; color: red");
     }
 
     if (spinner.style.left === "50%") {
-      console.log(
-        "%c✓ spiner in position",
-        "padding: 0 15px;background: #000; color: #bada55"
-      );
-    } else {
-      console.log(
-        "%c✗ spiner is not in position",
-        "padding: 0 15px;background: #000; color: red"
-      );
+      console.log("%c✓ spiner in position","padding: 0 15px;background: #000; color: #bada55");
+    } 
+    else {
+      console.log("%c✗ spiner is not in position","padding: 0 15px;background: #000; color: red");
     }
 
     if (loader.classList.contains("active")) {
-      console.log(
-        "%c✓ loader is active",
-        "padding: 0 15px;background: #000; color: #bada55"
-      );
-    } else {
-      console.log(
-        "%c✗ loader is not in active",
-        "padding: 0 15px;background: #000; color: red"
-      );
+      console.log("%c✓ loader is active","padding: 0 15px;background: #000; color: #bada55");
+    }
+     else {
+      console.log("%c✗ loader is not in active","padding: 0 15px;background: #000; color: red");
     }
 
     setTimeout(function () {
       if (body.style.overflow === "scroll") {
-        console.log(
-          "%c✓ body is unlocked",
-          "padding: 0 15px;background: #000; color: #bada55"
-        );
+        console.log("%c✓ body is unlocked","padding: 0 15px;background: #000; color: #bada55");
       }
 
       if (loader.style.right === "100%") {
-        console.log(
-          "%c✓ loader in position",
-          "padding: 0 15px;background: #000; color: #bada55"
-        );
+        console.log("%c✓ loader in position","padding: 0 15px;background: #000; color: #bada55");
       }
 
       if (spinner.style.left === "-50%") {
-        console.log(
-          "%c✓ spiner in position",
-          "padding: 0 15px;background: #000; color: #bada55"
-        );
+        console.log("%c✓ spiner in position","padding: 0 15px;background: #000; color: #bada55");
       }
 
       if (!loader.classList.contains("active")) {
-        console.log(
-          "%c✓ loader is desactivate",
-          "padding: 0 15px;background: #000; color: #bada55"
-        );
+        console.log("%c✓ loader is desactivate","padding: 0 15px;background: #000; color: #bada55");
       }
     }, 2000);
   }
